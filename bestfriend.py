b652e16292dd5ae753b02f69d2801a0d46914821
Maddy Zainea <Madisonwenberg@gmail.com>
2022-12-05 13:50:04 -0500

    Another weird commit

--- bestfriend.py
+++ bestfriend.py
@@ -1,4 +1,16 @@
-first_name = input("What is your name? ")
-print(f"Hello {first_name}")
-best = input("Who is the best? ")
-print(f"NO! {best} is not the best, I am the best!")
+print("Welcome to the world of Pokemon!")
+starter = input("Choose your starter! Charmander  Squirtle  Bulbasaur: ")
+print(f"You choose {starter} ")
+confirm = input ("Is this the Pokemon you want to start your adventure with? ")
+if confirm == "Yes":
+    print("Fantastic! Take these")
+    print("You gained 10 Pokeballs!")
+else:
+    print("Choose carefully!")
+    mistake = input("Choose your starter! Charmander  Squirtle  Bulbasaur: ")
+    print(f"You choose {mistake} ")
+confirm = input ("Is this the Pokemon you want to start your adventure with? ")
+if confirm == "Yes":
+    print("Fantastic! Take these")
+    print("You gained 10 Pokeballs!")
+    
